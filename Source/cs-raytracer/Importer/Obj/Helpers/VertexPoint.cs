11780f81b1ecb704a99732c6cb11689dde7c58a6
Adam Martula <martulaadam@gmail.com>
2025-10-07 23:33:19 +0200

    Added a draft of an .obj importer
    Add initial implementation of 3D model importer with FaceDescription, ObjectDescription, SceneDescription, and VertexPoint classes

--- Source/cs-raytracer/Importer/Obj/Helpers/VertexPoint.cs
+++ Source/cs-raytracer/Importer/Obj/Helpers/VertexPoint.cs
@@ -1,11 +1,33 @@
-namespace TracerUIMockup.Importer.Obj.Helpers;
+namespace cs_raytracer.Importer.Obj.Helpers;
 
+/// <summary>
+/// Represents a single point in 3D space that defines part of a 3D model's geometry.
+/// Vertices are the building blocks that get connected by faces to form the surface of 3D objects.
+/// Immutable once created to ensure coordinate data integrity during ray tracing calculations.
+/// </summary>
+/// <param name="coordinates">Array containing [x, y, z] coordinates in 3D world space</param>
 public class VertexPoint(double[] coordinates)
 {
+    /// <summary>
+    /// X-axis position in 3D world space (typically left/right movement)
+    /// </summary>
     private readonly double _x = coordinates[0];
+    
+    /// <summary>
+    /// Y-axis position in 3D world space (typically up/down movement)
+    /// </summary>
     private readonly double _y = coordinates[1];
+    
+    /// <summary>
+    /// Z-axis position in 3D world space (typically forward/backward depth)
+    /// </summary>
     private readonly double _z = coordinates[2];
 
+    /// <summary>
+    /// Retrieves the complete 3D coordinates of this vertex point.
+    /// Used by rendering systems and face definitions to access position data.
+    /// </summary>
+    /// <returns>Array containing [x, y, z] coordinates in that exact order</returns>
     public double[] GetCoordinates()
     {
         return [_x, _y, _z];
--- Source/cs-raytracer/Importer/Obj/ObjImporter.cs
+++ Source/cs-raytracer/Importer/Obj/ObjImporter.cs
@@ -1,24 +1,29 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using cs_raytracer.Importer.Obj.Helpers;
 
-namespace TracerUIMockup.Importer.Obj;
+namespace cs_raytracer.Importer.Obj;
 
 public class ObjImporter
 {
-    public SceneDescription ImportModel(string filePath)
+    /// <summary>
+    /// Parses a Wavefront .obj file and converts it into our internal 3D scene representation
+    /// </summary>
+    /// <param name="filePath">Path to the .obj file to import</param>
+    /// <returns>SceneDescription containing all vertices, faces, and objects from the file</returns>
+    public SceneDescription ImportModel(string? filePath)
     {
-        // Split the file into sections based on the models specified within
+        // Parse the .obj file and separate it into individual 3D objects
         var wavefrontObjects = SplitFileByObjects(filePath);
 
         // Create a description for the scene
         var sceneDescription = new SceneDescription(Path.GetFileName(filePath));
         
+        // Process each 3D object found in the file
         foreach (var wavefrontObject in wavefrontObjects)
         {
-            // Create an object description
+            // Create a container for this object's geometry data
             var objectDescription = new ObjectDescription();
 
             foreach (var line in wavefrontObject)
@@ -32,60 +37,61 @@ public class ObjImporter
                 // Get the position of the first space to determine where the first word ends
                 var firstSpaceIndex = line.IndexOf(' ');
                 
-                // Get the first word of the line to determine what type it is
+                // Extract the .obj command type (o, v, f, etc.)
                 var firstWord = line.Substring(0, firstSpaceIndex);
 
-                // Filter the file sections for vertices and faces
+                // Parse different types of .obj file data based on the command
                 switch (firstWord)
                 {
+                    // Object command found
                     case "o":
-                        // Remove the tag
+                        // Object name definition - extract everything after "o "
                         var nameOnly = line[(firstSpaceIndex + 1)..];
                         objectDescription.objectName = nameOnly;
                         break;
-                    // Vertex found
+                    // Vertex command found
                     case "v":
-                        // Remove the v tag to only have the coordinates
+                        // Vertex definition - parse 3D coordinates (x, y, z)
                         var coordinatesOnly = line[(firstSpaceIndex + 1)..];
                         
-                        // Split the line into the coordinates
+                        // Split "x y z" into individual coordinate strings
                         var splitStringCoordinates = coordinatesOnly.Split(' ');
                         
-                        // Assign X,Y and Z to the corresponding index
+                        // Convert coordinate strings to numbers
                         var coordinates = new double[3];
                         
-                        // Use InvariantCulture to avoid problems with culture number parsing (0,5 or 0.5)
+                        // Use InvariantCulture to handle decimal points correctly regardless of system locale
                         coordinates[0] = double.Parse(splitStringCoordinates[0], CultureInfo.InvariantCulture);
                         coordinates[1] = double.Parse(splitStringCoordinates[1], CultureInfo.InvariantCulture);
                         coordinates[2] = double.Parse(splitStringCoordinates[2], CultureInfo.InvariantCulture);
                         
-                        // Add the vertex point to the global vertex point list
+                        // Store this vertex in our scene's master vertex list
                         sceneDescription.VertexPoints?.Add(new VertexPoint(coordinates));
                         break;
-                    // Face found
+                    // Face command found
                     case "f":
-                        // Remove the f tag to only have the indices
+                        // Face definition - defines which vertices connect to form a polygon
                         var indicesOnly = line[(firstSpaceIndex + 1)..];
                         
-                        // Split the line by each index group
+                        // Split into individual vertex references
                         var splitStringIndices = indicesOnly.Split(' ');
                         
                         var indices = new List<int>();
 
-                        // Indices might contain v/vt/vn info, only use the first element
+                        // Extract just the vertex indices (ignore texture/normal data after '/')
                         foreach (var index in splitStringIndices)
                         {
                             var vertexIndexOnly = index.Split('/');
                             indices.Add(int.Parse(vertexIndexOnly[0]));
                         }
                         
-                        // Add the indices
+                        // Add this face to the current object
                         objectDescription.FacePoints.Add(new FaceDescription(indices));
                         break;
                 }
             }
             
-            // Add the objectDescription to the SceneDescription
+            // Add this completed object to our scene
             sceneDescription.ObjectDescriptions?.Add(objectDescription);
         }
         
@@ -93,41 +99,40 @@ public class ObjImporter
     }
 
     /// <summary>
-    /// Method for splitting the file into sections based on objects in that file
+    /// Splits a .obj file into separate sections, one for each 3D object defined in the file
     /// </summary>
-    /// <param name="filePath">File to be split</param>
-    /// <returns>A list of sections containing all the relevant information for each object</returns>
+    /// <param name="filePath">Path to the .obj file to process</param>
+    /// <returns>List of line groups, where each group contains all data for one 3D object</returns>
     private List<List<string>> SplitFileByObjects(string filePath)
     {
                 
-        // Open up a stream-reader at the specified path
+        // Open the file for reading
         using var streamReader = new StreamReader(filePath);
         
-        // Split the file by objects
+        // Track how many objects we've found
         var objectCount = 0;
-        // Create a list for the objects itself
+        // Store each object as a separate list of lines
         var objects = new List<List<string>>();
-        // Save the lines from the currently read section
+        // Collect lines for the current object being processed
         var currentFileSection = new List<string>();
         
-        // Read all lines from the file
+        // Process the file line by line
         string? line;
         while ((line = streamReader.ReadLine()) != null)
         {
-            // If the line is empty, skip it
+            // Skip empty lines
             if (string.IsNullOrEmpty(line))
             {
                 continue;    
             }
             
-            // Get the first word of the line to determine what type it is
+            // Check what type of .obj command this line contains
             var firstWord = line.Substring(0, line.IndexOf(' '));
             
-            // o is in the beginning of the file, a new object was found
+            // "o" command starts a new 3D object definition
             if (firstWord == "o")
             {
-                // Check if it is the first occurence of the o tag
-                // If it is, then skip adding it to the list, because everything before the first o tag is not an object
+                // If this isn't the first object, save the previous object's data
                 if (objectCount != 0)
                 {
                     objects.Add(currentFileSection);
@@ -136,14 +141,14 @@ public class ObjImporter
                 // Up the object count
                 ++objectCount;
                 
-                // Create a new list to start adding lines for the new object
+                // Start collecting lines for this new object
                 currentFileSection = [];
             }
             
-            // Add the current line to the current file section
+            // Add this line to the current object's data
             currentFileSection.Add(line);
         }
-        // Add the last object
+        // Don't forget to add the final object when we reach the end of the file
         objects.Add(currentFileSection);
 
         return objects;
