35621d67e32f680b0f81c9ceef2ed2ed79a9e798
Adam Martula <martulaadam@gmail.com>
2025-10-08 11:33:27 +0200

    Renamed project

--- Source/GOATracer/App.axaml.cs
+++ Source/GOATracer/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace cs_raytracer;
+namespace GOATracer;
 
 public partial class App : Application
 {
